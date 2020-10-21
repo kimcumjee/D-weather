435b00c250cd9281ce454043046a6baabb8d11fd
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-10-21 19:53:25 +0900

    [Development]MVVM
    MVVM패턴 적용

--- app/src/main/java/com/e/d_weather/network/GetWeather.kt
+++ app/src/main/java/com/e/d_weather/network/GetWeather.kt
@@ -14,28 +14,19 @@ class GetWeather {
     internal fun getWeather(
         application: Application
     ) {
-
+        val appId = "09c8dfc52b7541d33c528d09a55e2c18"
+        val locate = "DAEGU"
         (application as RetrofitApplication).retrofit.create(WeatherInterface::class.java)
-            .getWeather()
-
+            .getWeather(appId,locate)
             .enqueue(object : Callback<WeatherDataClass> {
-
                 override fun onResponse(
-                    call: Call<EmailLoginBody>,
-                    response: Response<EmailLoginBody>
-
+                    call: Call<WeatherDataClass>,
+                    response: Response<WeatherDataClass>
                 ) {
-                    //다음 화면으로 이동
-                    val intent = Intent(context, MainActivity::class.java)
 
-                    
                     //통신성공
                     if (response.code() == 200) {
-
-                        //서버로부터 받은 정보들을 EmailLoginBody 변수에 담아준다
-                        EmailLoginBody.instance = response.body()
-                        getUser.getUserData(getApplication, context)
-
+                        Log.d("success","success : ${response.body()}")
                     }
                     //통신 실패
                     else if (response.code() == 401) {
@@ -46,13 +37,11 @@ class GetWeather {
                 }
 
                 //서버와 연결 실패
-                override fun onFailure(call: Call<EmailLoginBody>, t: Throwable) {
-                    //LoginDialog 를 호출하여 서버와의 연결 실패를 dialog 로 띄운다
-                    loginDialog.connectionFail(context, sweetAlertDialog)
+                override fun onFailure(call: Call<WeatherDataClass>, t: Throwable) {
+
                 }
 
 
             })
     }
-}
 }
